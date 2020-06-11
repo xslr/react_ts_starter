813b1e056ffd4324076b3e578221eff1bd44024d
Subrat Meher <smeher02.aoe3@gmail.com>
2020-06-11 11:36:46 +0200

    Add css support

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,14 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./component/Hello";
+import { Game } from "./component/TicTacToe";
 
 ReactDOM.render(
     <Hello compiler="TypeScript" framework="React" />,
     document.getElementById("tsExample")
+);
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('tictactoe-root')
 );
