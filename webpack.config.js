a6f57a4af9cd2e0e82eea1ce99db72d761efd3b5
Subrat Meher <smeher02.aoe3@gmail.com>
2020-06-13 08:37:14 +0200

    Add css webpack tooling

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,26 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  devtool: "source-map",
+  devtool: 'source-map',
 
   entry: './src/index.tsx',
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx', '.css', '.scss']
+  },
   module: {
     rules: [
       {
-        enforce: "pre",
+        test: /\.(s?)css/,
+        include: path.resolve(__dirname, 'src'),
+        use: [
+          { loader: "style-loader" },  // to inject the result into the DOM as a style block
+          { loader: "css-modules-typescript-loader"},  // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+          { loader: "css-loader", options: { modules: true } },  // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+          { loader: "sass-loader" },  // to convert SASS to CSS
+        ]
+      },
+      {
+        enforce: 'pre',
         test: /\.js(x?)$/,
         use: ['source-map-loader']
       },
@@ -27,9 +40,6 @@ module.exports = {
         ]
       }
     ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx', '.ts', '.tsx']
   },
   output: {
     filename: 'bundle.js',
